fdb440925901c309faef69a6dbe3e6ce771bb7af
DAMAVISHNUVARDHAN <112301447+DAMAVISHNUVARDHAN@users.noreply.github.com>
2024-05-02 08:17:36 +0530

    Update Exp1_Halfadder.v

--- Exp1_Halfadder.v
+++ Exp1_Halfadder.v
@@ -1,8 +1,9 @@
-module halfsubractor(a,b,diff,borrow);
+module halfadder(a,b,m,c);
 input a,b;
-output diff,borrow;
-wire w1;
-xor g1(diff,a,b);
-not g2(w1,a);
-and g3(borrow,w1,b);
+output m,c;
+xor g1(m,a,b);
+and g2(c,a,b);
+
+
+
 endmodule
