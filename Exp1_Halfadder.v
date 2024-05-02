1ff7aebb39d8c32700f24137506bbf2b0f55d166
DAMAVISHNUVARDHAN <112301447+DAMAVISHNUVARDHAN@users.noreply.github.com>
2024-05-02 08:15:57 +0530

    Update Exp1_Halfadder.v

--- Exp1_Halfadder.v
+++ Exp1_Halfadder.v
@@ -1,5 +1,8 @@
-module ha(a,b,sum,carry);
+module halfsubractor(a,b,diff,borrow);
 input a,b;
-output sum,carry;
-
+output diff,borrow;
+wire w1;
+xor g1(diff,a,b);
+not g2(w1,a);
+and g3(borrow,w1,b);
 endmodule
