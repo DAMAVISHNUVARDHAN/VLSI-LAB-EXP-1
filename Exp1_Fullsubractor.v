b79f48619625276a8408bf6c423fd53c55efc30c
DAMAVISHNUVARDHAN <112301447+DAMAVISHNUVARDHAN@users.noreply.github.com>
2024-05-02 08:15:35 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,8 +1,6 @@
-module halfsubractor(a,b,diff,borrow);
-input a,b;
-output diff,borrow;
-wire w1;
-xor g1(diff,a,b);
-not g2(w1,a);
-and g3(borrow,w1,b);
+module full_subtractor(a, b,c,D, bout);
+input a, b, c;
+output D, bout;
+assign D= a ^ b ^ c;
+assign bout= (~a&b) | (~(a ^ b) & c);
 endmodule
