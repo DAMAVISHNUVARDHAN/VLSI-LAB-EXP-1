f74302c812228c050d1dbf822f347e4bc1e7e28a
DAMAVISHNUVARDHAN <112301447+DAMAVISHNUVARDHAN@users.noreply.github.com>
2024-05-02 08:13:30 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,8 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
-
-
-
+module halfsubractor(a,b,diff,borrow);
+input a,b;
+output diff,borrow;
+wire w1;
+xor g1(diff,a,b);
+not g2(w1,a);
+and g3(borrow,w1,b);
 endmodule
